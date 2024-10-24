949a84448d7fc0cdc382b2ffe82b1e1c778cae92
Shashank Kumar <shashank.kumar.phe22@itbhu.ac.in>
2024-10-24 19:33:35 +0530

    convert to kotlin

--- codegen/src/main/java/org/web3j/codegen/unit/gen/java/JavaClassGenerator.kt
+++ codegen/src/main/java/org/web3j/codegen/unit/gen/java/JavaClassGenerator.kt
@@ -10,57 +10,50 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.codegen.unit.gen.java;
+package org.web3j.codegen.unit.gen.java
 
-import java.io.File;
-import java.io.IOException;
-import javax.lang.model.element.Modifier;
-
-import com.squareup.javapoet.AnnotationSpec;
-import com.squareup.javapoet.ClassName;
-import com.squareup.javapoet.JavaFile;
-import com.squareup.javapoet.TypeSpec;
-
-import org.web3j.codegen.unit.gen.MethodFilter;
-import org.web3j.codegen.unit.gen.UnitClassGenerator;
-import org.web3j.commons.JavaVersion;
-
-import static org.web3j.codegen.unit.gen.utils.NameUtils.toCamelCase;
+import com.squareup.javapoet.AnnotationSpec
+import com.squareup.javapoet.ClassName
+import com.squareup.javapoet.TypeSpec
+import org.web3j.codegen.unit.gen.MethodFilter
+import org.web3j.commons.JavaVersion
+import java.io.File
+import java.io.IOException
+import javax.lang.model.element.Modifier
 
 /**
  * Class that generates the unit tests classes for the contracts. The class writes to
  * src/test/java/contracts and each file is named after the contract + "Test" e.g GreeterTest
  */
-public class JavaClassGenerator implements UnitClassGenerator {
-    private final Class theContract;
-    private final String packageName;
-    private final String writePath;
-
-    public JavaClassGenerator(final Class theContract, final String packageName, String writePath) {
-        this.theContract = theContract;
-        this.packageName = packageName;
-        this.writePath = writePath;
-    }
-
-    @Override
-    public void writeClass() throws IOException {
-        ClassName EVM_ANNOTATION = ClassName.get("org.web3j", "EVMTest");
-        AnnotationSpec.Builder annotationSpec = AnnotationSpec.builder(EVM_ANNOTATION);
+class JavaClassGenerator(
+    private val theContract: Class<*>,
+    private val packageName: String,
+    private val writePath: String
+) : UnitClazssGenerator {
+    @Throws(IOException::class)
+    override fun writeClass() {
+        val EVM_ANNOTATION = ClassName.get("org.web3j", "EVMTest")
+        val annotationSpec = AnnotationSpec.builder(EVM_ANNOTATION)
         if (JavaVersion.getJavaVersionAsDouble() < 11) {
-            ClassName GethContainer = ClassName.get("org.web3j", "NodeType");
-            annotationSpec.addMember("value", "type = $T.GETH", GethContainer);
+            val GethContainer = ClassName.get("org.web3j", "NodeType")
+            annotationSpec.addMember("value", "type = \$T.GETH", GethContainer)
         }
-        TypeSpec testClass =
-                TypeSpec.classBuilder(theContract.getSimpleName() + "Test")
-                        .addMethods(MethodFilter.generateMethodSpecsForEachTest(theContract))
-                        .addAnnotation((annotationSpec).build())
-                        .addField(
-                                theContract,
-                                toCamelCase(theContract),
-                                Modifier.PRIVATE,
-                                Modifier.STATIC)
-                        .build();
-        JavaFile javaFile = JavaFile.builder(packageName, testClass).build();
-        javaFile.writeTo(new File(writePath));
+        val testClass =
+            TypeSpec.classBuilder(theContract.simpleName + "Test")
+                .addMethods(
+                    MethodFilter.generateMethodSpecsForEachTest(
+                        theContract
+                    )
+                )
+                .addAnnotation(annotationSpec.build())
+                .addField(
+                    theContract,
+                    NameUtils.toCamelCase(theContract),
+                    Modifier.PRIVATE,
+                    Modifier.STATIC
+                )
+                .build()
+        val javaFile: JavaFile = JavaFile.builder(packageName, testClass).build()
+        javaFile.writeTo(File(writePath))
     }
 }
--- codegen/src/main/java/org/web3j/codegen/unit/gen/java/MethodParser.kt
+++ codegen/src/main/java/org/web3j/codegen/unit/gen/java/MethodParser.kt
@@ -10,88 +10,78 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.codegen.unit.gen.java;
+package org.web3j.codegen.unit.gen.java
 
-import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-import javax.lang.model.element.Modifier;
-
-import com.squareup.javapoet.MethodSpec;
-import com.squareup.javapoet.ParameterSpec;
-import org.junit.jupiter.api.BeforeAll;
-
-import org.web3j.codegen.unit.gen.utils.JavaMappingHelper;
-import org.web3j.protocol.Web3j;
-import org.web3j.tx.TransactionManager;
-import org.web3j.tx.gas.ContractGasProvider;
-
-import static org.web3j.codegen.unit.gen.utils.NameUtils.toCamelCase;
+import com.squareup.javapoet.ParameterSpec
+import org.junit.jupiter.api.BeforeAll
+import java.lang.reflect.Method
+import java.util.Arrays
+import java.util.stream.Collectors
+import java.util.stream.Stream
+import javax.lang.model.element.Modifier
 
 /*Class that when given a method provides a JavaPoet method spec. */
-public class MethodParser {
-    private final Method method;
-    private final Class theContract;
-    private final String uniqueMethodName;
+class MethodParser(
+    private val method: Method,
+    private val theContract: Class<*>,
+    private val uniqueMethodName: String
+) {
+    val methodSpec: MethodSpec
+        get() = if (methodNeedsInjection()
+        ) MethodSpecGenerator(
+            uniqueMethodName,
+            BeforeAll::class.java,
+            Modifier.STATIC,
+            defaultParameterSpecsForEachUnitTest(),
+            generateStatementBody()
+        )
+            .generate()
+        else MethodSpecGenerator(uniqueMethodName, generateStatementBody()).generate()
 
-    public MethodParser(final Method method, final Class theContract, String uniqueMethodName) {
-        this.method = method;
-        this.theContract = theContract;
-        this.uniqueMethodName = uniqueMethodName;
+    private fun methodNeedsInjection(): Boolean {
+        return Arrays.asList<Class<*>>(*method.parameterTypes)
+            .containsAll(
+                Arrays.asList<Class<out Any>>(
+                    Web3j::class.java,
+                    TransactionManager::class.java,
+                    ContractGasProvider::class.java
+                )
+            )
     }
 
-    public MethodSpec getMethodSpec() {
-        return methodNeedsInjection()
-                ? new MethodSpecGenerator(
-                                uniqueMethodName,
-                                BeforeAll.class,
-                                Modifier.STATIC,
-                                defaultParameterSpecsForEachUnitTest(),
-                                generateStatementBody())
-                        .generate()
-                : new MethodSpecGenerator(uniqueMethodName, generateStatementBody()).generate();
+    private fun defaultParameterSpecsForEachUnitTest(): List<ParameterSpec> {
+        return Stream.of<ParameterSpec>(
+            ParameterSpec.builder(Web3j::class.java, NameUtils.toCamelCase(Web3j::class.java))
+                .build(),
+            ParameterSpec.builder(
+                TransactionManager::class.java,
+                NameUtils.toCamelCase(TransactionManager::class.java)
+            )
+                .build(),
+            ParameterSpec.builder(
+                ContractGasProvider::class.java,
+                NameUtils.toCamelCase(ContractGasProvider::class.java)
+            )
+                .build()
+        )
+            .collect(Collectors.toList<ParameterSpec>())
     }
 
-    private boolean methodNeedsInjection() {
-        return Arrays.asList(method.getParameterTypes())
-                .containsAll(
-                        Arrays.asList(
-                                Web3j.class, TransactionManager.class, ContractGasProvider.class));
-    }
-
-    private List<ParameterSpec> defaultParameterSpecsForEachUnitTest() {
-        return Stream.of(
-                        ParameterSpec.builder(Web3j.class, toCamelCase(Web3j.class)).build(),
-                        ParameterSpec.builder(
-                                        TransactionManager.class,
-                                        toCamelCase(TransactionManager.class))
-                                .build(),
-                        ParameterSpec.builder(
-                                        ContractGasProvider.class,
-                                        toCamelCase(ContractGasProvider.class))
-                                .build())
-                .collect(Collectors.toList());
-    }
-
-    private Map<String, Object[]> generateStatementBody() {
-        Map<String, Object[]> methodBodySpecification = new LinkedHashMap<>();
-        JavaParser parser = new JavaParser(theContract, method, new JavaMappingHelper());
-        String javaPoetStringTypes = parser.generatePoetStringTypes();
-        Object[] genericParameters = parser.generatePlaceholderValues();
-        methodBodySpecification.put(javaPoetStringTypes, genericParameters);
+    private fun generateStatementBody(): Map<String, Array<Any>> {
+        val methodBodySpecification: MutableMap<String, Array<Any>> = LinkedHashMap()
+        val parser = JavaParser(theContract, method, JavaMappingHelper())
+        val javaPoetStringTypes = parser.generatePoetStringTypes()
+        val genericParameters = parser.generatePlaceholderValues()
+        methodBodySpecification[javaPoetStringTypes] = genericParameters
         if (methodNeedsAssertion()) {
-            String assertionJavaPoet = parser.generateAssertionJavaPoetStringTypes();
-            Object[] assertionParams = parser.generateAssertionPlaceholderValues();
-            methodBodySpecification.put(assertionJavaPoet, assertionParams);
+            val assertionJavaPoet = parser.generateAssertionJavaPoetStringTypes()
+            val assertionParams = parser.generateAssertionPlaceholderValues()
+            methodBodySpecification[assertionJavaPoet] = assertionParams
         }
-        return methodBodySpecification;
+        return methodBodySpecification
     }
 
-    private boolean methodNeedsAssertion() {
-        return !methodNeedsInjection();
+    private fun methodNeedsAssertion(): Boolean {
+        return !methodNeedsInjection()
     }
 }
